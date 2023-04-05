29ba1ccee360d5ec15897dc27a2baa7c32686d88
anvuvip2002 <nguyenphuocanvu@gmail.com>
2023-04-05 11:27:17 +0700

    component header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,43 +1,116 @@
-function Header () {
-    return (     
-        //test thử bootstrap       
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './Header.module.css'
+import { NavLink } from 'react-bootstrap';
+//material icons
+import PhoneIcon from '@mui/icons-material/Phone';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import HistoryIcon from '@mui/icons-material/History';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+    const menuTop = [
+      {
+        path:'/phone',
+        name: "113 567 900",
+        icon: <PhoneIcon/>
+    },
+    
+    {
+        path:'/calendar',
+        name: "Đặt lịch hẹn",
+        icon: <CalendarTodayIcon/>
+    },
+    {
+        path:'/transactions',
+        name: "Lịch sử đơn hàng",
+        icon: <HistoryIcon/>
+    },
+    {
+        path:'/orders',
+        name: "Giỏ hàng",
+        icon: <ShoppingCartIcon/>
+    },
+    ]
+    const menuPages = [
+        {
+            path:'/',
+            name: "Trang chủ"
+        },
+        {
+            path:'/products',
+            name: "Sản phẩm"
+        },
+        {
+            path:'/blog',
+            name: "Blog"
+        },
+        {
+            path:'/about',
+            name: "Về chúng tôi"
+        },
+        {
+            path:'/account',
+            name: "Tài khoản"
+        },
+    ]
+  return (
+    <>
+    <Navbar className='navbar-header' expand='lg'>
+        <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <div className="item">
+            <img
+              src={require('../../assets/images/logo.png')}
+              alt=""
+              className="logo"
+            />
+          </div>
+          <Nav className="me-auto">
+          {
+                menuTop.map((item)=>(
+                    <NavLink to={item.path} key={item.name}>
+                      <div className='items'>
+                      <div className='icon'>{item.icon}</div>
+                        <div className='item'>{item.name}</div>
+                        </div>
+                    </NavLink>
+                ))
+            }
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    
+
+    <Navbar className='navbar-main' expand='lg'>
+      <Container>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {
+                menuPages.map((item)=>(
+                    <NavLink to={item.path} key={item.name}>
+                        <div className='list_item'>{item.name}</div>
+                    </NavLink>
+                ))
+            }
+          </Nav>
+          <Nav className='ma-auto'>
                 <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
+                    <input className="form-control me-2" type="search" placeholder="Tìm kiếm nhanh" aria-label="Search" size="100"/>
+                    <button className="btn btn-outline-success" type="submit"><SearchIcon/></button>
                 </form>
-                </div>
-            </div>
-        </nav>
-    );
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  );
 }
 
 export default Header;
