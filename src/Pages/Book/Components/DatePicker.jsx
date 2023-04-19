25b5976c32738e4ab5cc4a81adae10d51d2a0add
your name <you@exemple.com>
2023-04-19 23:50:08 +0700

    book

--- src/Pages/Book/Components/DatePicker.jsx
+++ src/Pages/Book/Components/DatePicker.jsx
@@ -9,13 +9,13 @@ const Datepicker = () => {
   const [selectedDate, setSelectedDate] = useState(null);
 
   return (
-    <Form style={{width : "1000px"}} className="align-items-center justify-content-center">
-    <Form.Group controlId="formBasicDate" className="align-items-center justify-content-center">
-      <Row> 
-          <Col md={4}>
+    <Form style={{ border:'solid 2px'}} className="align-items-center justify-content-center mt-3" >
+    <Form.Group controlId="formBasicDate" className="align-items-center justify-content-center mt-3" >
+      <Row className="align-items-center justify-content-center"> 
+          <Col md={2}>
               <Form.Label>Ngày hẹn *</Form.Label>
           </Col>
-          <Col md={8}>
+          <Col md={4}>
               <DatePicker
               selected={selectedDate}
               onChange={(date) => setSelectedDate(date)}
@@ -23,38 +23,60 @@ const Datepicker = () => {
               />
           </Col>
       </Row>
-      <Row>
-          <Col  md={4}>
+      <Row className="align-items-center justify-content-center">
+          <Col  md={2}>
               <Form.Label>Sản phẩm cần tư vấn *</Form.Label>
           </Col>
-          <Col  md={8}>
-              <Form.Select aria-label="Default select example">
+          <Col  md={4}>
+          {['checkbox'].map((type) => (
+          <div key={`default ${type}`} className="mt-3">
+            <Row>
+              <Col>
+                <Form.Check 
+                type={type}
+                id={`default`}
+                label={`Nhẫn`}
+                />
+                <Form.Check 
+                type={type}
+                id={`default`}
+                label={`Bông tai`}
+                />
+                <Form.Check 
+                type={type}
+                id={`default`}
+                label={`Dây chuyền`}
+                />
+              </Col>
+            </Row>
+            
+          </div>
+        ))}
+              {/* <Form.Select aria-label="Default select example">
                   <option>Chọn sản phẩm</option>
                   <option value="1">Nhẫn</option>
                   <option value="2">Dây chuyền</option>
                   <option value="3">Bông tai</option>
-                  <option value="4">Vòng tay</option>
-              </Form.Select>
+              </Form.Select> */}
           </Col>
       </Row>
-      <Row>
-          <Col md={4}>
+      <Row className="align-items-center justify-content-center">
+          <Col md={2}>
               <Form.Label>Lời nhắn</Form.Label>
           </Col>
-          <Col md={8}>
+          <Col md={4} className='mt-3'>
           <Form.Control 
               as="textarea"
               rows={3}
               placeholder="Nội dung lời nhắn"
               />
-            <div style={{ marginTop: '2rem' }} className="text-center">
+           
+          </Col>
+      </Row>
+      <div style={{ marginTop: '2rem' }} className="text-center mb-3">
                 
                 <Button variant="primary">Gửi</Button>{' '}
             </div>
-          </Col>
-      </Row>
-       
-      
     </Form.Group>
   </Form>
   
