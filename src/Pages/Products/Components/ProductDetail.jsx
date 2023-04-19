f654daf6ad131961f995b3c650a7a9a7fec98b6d
your name <you@exemple.com>
2023-04-20 00:24:25 +0700

    productdetail

--- src/Pages/Products/Components/ProductDetail.jsx
+++ src/Pages/Products/Components/ProductDetail.jsx
@@ -4,50 +4,84 @@ import { Form, FormControl} from 'react-bootstrap';
 function Product() {
   return (
     <Container>
-      <Row>
-        <Col md={6}>
+      <Row className="align-items-center justify-content-center">
+        <Col md={4}>
           <Image src="https://cdn.pnj.io/images/detailed/153/sbpmxmw000001-bong-tai-bac-dinh-ngoc-trai-pnjsilver-1.png" fluid />
         </Col>
-        <Col md={6}>
+        <Col md={6} >
           <h1>Bông tai Bạc đính Ngọc Trai PNJSilver PMXMW000001</h1>
           <p>Mã: SBPMXMW000001</p>
+          <Row>
+            <Col>
+            <p>Đã bán: 4</p>
+            </Col>
+            <Col>
+            <p>Còn lại: 5</p>
+            </Col>
+          </Row>
+          
+          
           <h2>795.000 đ</h2>
           <div className="d-flex align-items-center mb-3">
             <span className="me-3">Số lượng:</span>
             <Button variant="outline-secondary" size="sm">
               -
             </Button>
-            <span className="mx-3"></span>
+            <span className="mx-3">1</span>
             <Button variant="outline-secondary" size="sm">
               +
             </Button>
           </div>
-          <Button variant="primary" size="lg" className="mb-3">
+          <Button variant="primary" size="md" className="mb-3">
             Mua hàng <br /> <i>Miễn phí giao hàng</i>
           </Button>
         </Col>
       </Row>
       <div>
-      <h3>Thông số và mô tả</h3>
-      <ListGroup>
-        <ListGroupItem>
-          <strong>Mã sản phẩm:</strong> PMXMW000001
-        </ListGroupItem>
-        <ListGroupItem>
-          <strong>Chất liệu:</strong> Bạc 925, ngọc trai
-        </ListGroupItem>
-        <ListGroupItem>
-          <strong>Kích thước:</strong> 1.5cm x 1.5cm
-        </ListGroupItem>
-        <ListGroupItem>
-          <strong>Trọng lượng:</strong> 2.5g
-        </ListGroupItem>
-        <ListGroupItem>
-          <strong>Giá:</strong> 1,200,000đ <Badge variant="danger">-10%</Badge>
-        </ListGroupItem>
-      </ListGroup>
+      <h3>Thông số</h3>
+      <Row>
+        <Col>
+          <ListGroup className="w-100">
+          <ListGroupItem>
+            <strong>Trọng lượng tham khảo: </strong> 4.36703phân
+          </ListGroupItem>
+          <ListGroupItem>
+            <strong>Loại đá chính: </strong> Kim cương
+          </ListGroupItem>
+          <ListGroupItem>
+            <strong>Kích thước đá chính (tham khảo): </strong> 3.3ly
+          </ListGroupItem>
+          <ListGroupItem>
+            <strong>Hình dạng đá: </strong> Trái tim
+          </ListGroupItem>
+          {/* <ListGroupItem>
+            <strong>Giá:</strong> 1,200,000đ <Badge variant="danger">-10%</Badge>
+          </ListGroupItem> */}
+        </ListGroup>
+        </Col>
+        <Col>
+          <ListGroup>
+          <ListGroupItem>
+            <strong>Loại đá phụ: </strong> Kim cương
+          </ListGroupItem>
+          <ListGroupItem>
+            <strong>Màu đá phụ: </strong> Trắng
+          </ListGroupItem>
+          <ListGroupItem>
+            <strong>Số viên đá phụ: </strong> 26 viên
+          </ListGroupItem>
+          <ListGroupItem>
+            <strong>Giới tính: </strong> Nữ
+          </ListGroupItem>
+          {/* <ListGroupItem>
+            <strong>Giá:</strong> 1,200,000đ <Badge variant="danger">-10%</Badge>
+          </ListGroupItem> */}
+        </ListGroup>
+        </Col>
+      </Row>
+      
     </div>
-    <h3>Bình luận</h3>
+    <h3 className="mt-3">Bình luận</h3>
     <Form>
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Nhập tên của bạn:</Form.Label>
@@ -59,7 +93,7 @@ function Product() {
         <FormControl as="textarea" placeholder="Nhập bình luận của bạn" />
       </Form.Group>
 
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" className="mt-3 mb-3">
         Gửi bình luận
       </Button>
     </Form>
