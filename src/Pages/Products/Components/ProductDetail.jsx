5c524e379e8c771698d480f47462edc63e0557cf
your name <you@exemple.com>
2023-04-22 14:55:54 +0700

    book and productdetail

--- src/Pages/Products/Components/ProductDetail.jsx
+++ src/Pages/Products/Components/ProductDetail.jsx
@@ -9,19 +9,19 @@ function Product() {
           <Image src="https://cdn.pnj.io/images/detailed/153/sbpmxmw000001-bong-tai-bac-dinh-ngoc-trai-pnjsilver-1.png" fluid />
         </Col>
         <Col md={6} >
-          <h1>Bông tai Bạc đính Ngọc Trai PNJSilver PMXMW000001</h1>
+          <h2>Bông tai Bạc đính Ngọc Trai PNJSilver PMXMW000001</h2>
           <p>Mã: SBPMXMW000001</p>
           <Row>
-            <Col>
+            <Col md={2}>
             <p>Đã bán: 4</p>
             </Col>
             <Col>
-            <p>Còn lại: 5</p>
+            <p>Còn hàng</p>
             </Col>
           </Row>
+         
           
-          
-          <h2>795.000 đ</h2>
+          <h4>Giá: 795.000 đ</h4>
           <div className="d-flex align-items-center mb-3">
             <span className="me-3">Số lượng:</span>
             <Button variant="outline-secondary" size="sm">
@@ -33,45 +33,26 @@ function Product() {
             </Button>
           </div>
           <Button variant="primary" size="md" className="mb-3">
-            Mua hàng <br /> <i>Miễn phí giao hàng</i>
+            Mua hàng <br /> 
           </Button>
         </Col>
       </Row>
       <div>
       <h3>Thông số</h3>
       <Row>
-        <Col>
+        <Col md={4}>
           <ListGroup className="w-100">
           <ListGroupItem>
-            <strong>Trọng lượng tham khảo: </strong> 4.36703phân
+            <strong>Chất liệu: </strong> Kim cương
           </ListGroupItem>
           <ListGroupItem>
-            <strong>Loại đá chính: </strong> Kim cương
+            <strong>Khối lượng: </strong> 4.36703 phân
           </ListGroupItem>
           <ListGroupItem>
-            <strong>Kích thước đá chính (tham khảo): </strong> 3.3ly
+            <strong>Kích thước: </strong> 3.3 ly
           </ListGroupItem>
           <ListGroupItem>
-            <strong>Hình dạng đá: </strong> Trái tim
-          </ListGroupItem>
-          {/* <ListGroupItem>
-            <strong>Giá:</strong> 1,200,000đ <Badge variant="danger">-10%</Badge>
-          </ListGroupItem> */}
-        </ListGroup>
-        </Col>
-        <Col>
-          <ListGroup>
-          <ListGroupItem>
-            <strong>Loại đá phụ: </strong> Kim cương
-          </ListGroupItem>
-          <ListGroupItem>
-            <strong>Màu đá phụ: </strong> Trắng
-          </ListGroupItem>
-          <ListGroupItem>
-            <strong>Số viên đá phụ: </strong> 26 viên
-          </ListGroupItem>
-          <ListGroupItem>
-            <strong>Giới tính: </strong> Nữ
+            <strong>Màu sắc: </strong> Trắng
           </ListGroupItem>
           {/* <ListGroupItem>
             <strong>Giá:</strong> 1,200,000đ <Badge variant="danger">-10%</Badge>
@@ -81,15 +62,15 @@ function Product() {
       </Row>
       
     </div>
-    <h3 className="mt-3">Bình luận</h3>
+    {/* <h3 className="mt-3">Bình luận</h3> */}
     <Form>
-      <Form.Group controlId="formBasicEmail">
+      {/* <Form.Group controlId="formBasicEmail">
         <Form.Label>Nhập tên của bạn:</Form.Label>
         <FormControl type="text" placeholder="Nhập tên của bạn" />
-      </Form.Group>
+      </Form.Group> */}
 
       <Form.Group controlId="formBasicPassword">
-        <Form.Label>Nhập bình luận của bạn:</Form.Label>
+        <Form.Label className="mt-4">Nhập bình luận của bạn:</Form.Label>
         <FormControl as="textarea" placeholder="Nhập bình luận của bạn" />
       </Form.Group>
 
