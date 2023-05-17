852966f86196f19f4a07118f22efb7af62455761
anvuvip2002 <nguyenphuocanvu@gmail.com>
2023-05-18 01:58:36 +0700

    api loai dich vu

--- src/admin/pages/service/addServiceType/addServiceType.jsx
+++ src/admin/pages/service/addServiceType/addServiceType.jsx
@@ -1,10 +1,38 @@
-
-
+import React, { useState } from "react";
+import axios from "axios";
 import styles from './addServiceType.module.css'
+import {useNavigate} from 'react-router-dom';
 const AddServiceType = ({ inputs}) => {
+  const [svt_id, setSvt_id] = useState("");
+  const [svt_name, setSvt_name] = useState("");
+  const [svt_price, setSvt_price] = useState("");
 
+  const navigate = useNavigate();
+  const navigateToServicePage = () => {
+    // 👇️ navigate to /contacts
+    navigate('/service');
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    try {
+      const response = await axios.post("http://localhost:3001/serviceType", {
+        svt_id,
+        svt_name,
+        svt_price,
 
+      });
+      console.log(response.data);
+      setSvt_id("");
+      setSvt_name("");
+
+      setSvt_price("");
+
+    } catch (error) {
+      console.error(error);
+    }
+    navigateToServicePage()
+  };
   return (
    
       <div className={styles.new}  style={{width:"1300px"}}>
@@ -20,22 +48,25 @@ const AddServiceType = ({ inputs}) => {
             <form>
               
               <div class={styles.formInput}><label>Mã Loại Vụ</label>
-              <input type="text" placeholder="VD: svt01"/>
+              <input type="text" value={svt_id}
+                onChange={(e) => setSvt_id(e.target.value)} placeholder="VD: svt01"/>
               </div>
 
               <div class={styles.formInput}><label>Tên Loại Dịch Vụ</label>
-              <input type="text" placeholder="VD: Mài Nữ Trang"/>
+              <input type="text" value={svt_name}
+                onChange={(e) => setSvt_name(e.target.value)} placeholder="VD: Mài Nữ Trang"/>
               </div>
 
               <div class={styles.formInput}><label>Giá</label>
-              <input type="number" placeholder="VD: 50000 VNĐ"/>
+              <input type="number" value={svt_price}
+                onChange={(e) => setSvt_price(e.target.value)} placeholder="VD: 50000 VNĐ"/>
               </div>
 
               
 
             </form>
             <div className={styles.buttonUpdate}>
-            <button className={styles.myButton}>Thêm</button>
+            <button type="submit" onClick={handleSubmit} className={styles.myButton}>Thêm</button>
             </div>
           </div>
           
--- src/admin/pages/service/service_admin.jsx
+++ src/admin/pages/service/service_admin.jsx
@@ -8,54 +8,47 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import styles from "./service.module.css";
 import Button from 'react-bootstrap/Button';
 
-// const columns = [
-  
-//   {
-//     field: "s_id",
-//     headerName: "Mã",
-//     width: 180,
-//   },
-//   {
-//     field: "s_name",
-//     headerName: "Tên Phiếu Dịch Vụ",
-//     width: 250,
-    
-//   },
-//   {
-//     field: "makh",
-//     headerName: "Khách Hàng",
-//     width: 250,
-//   }, 
-
-//   {
-//     field: "s_number",
-//     headerName: "Số Lượng",
-//     width: 250,
-//   },
-  
-//   {
-//     field: "s_date",
-//     headerName: "Ngày Lập",
-//     width: 250,
-    
-//   },
-// ]
 
 const Service = () => {
-  const [tableDataSVT, setTableDataSVT] = useState([])
+  //Service Types
+  const [tableDataSVT, setTableDataSVT] = useState([]) 
   
   useEffect(() => {
-    fetch("https://dialuxury.onrender.com/serviceType")
-      .then((data) => data.json())
-      .then((data) => setTableDataSVT(data))
+    loadSVT();
 
   }, [])
+
+  const loadSVT = async () => {
+    axios
+      .get("https://dialuxury.onrender.com/serviceType")
+      .then((response) => {
+        setTableDataSVT(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  function deleteSVT (id) 
+  {
+    fetch(`http://localhost:3001/serviceType/svtid/${id}`, {
+      method: 'DELETE'
+    }). then((result)=>{
+      result.json().then((resp)=>{
+        console.warn(resp)
+      })
+    })
+    loadSVT();
+  }
+
+  //Services
   
   const [tableData, setTableData] = useState([])
 
@@ -66,25 +59,7 @@ const Service = () => {
 
   }, [])
 
-  // const actionColumn = [
-  //   {
-  //     field: "action",
-  //     headerName: "Lựa Chọn",
-  //     width: 200,
-  //     renderCell: (params) => {
-  //       return (
-  //         <div className={styles.cellAction}>
-  //           <Link to="/service/view" style={{ textDecoration: "none" }}>
-  //             <div className={styles.viewButton}>View</div>
-  //           </Link>
-  //           <Button className={styles.deleteButton}>Delete</Button>
-            
 
-  //         </div>
-  //       );
-  //     },
-  //   },
-  // ];
 
 
   return (
@@ -98,33 +73,15 @@ const Service = () => {
         </Link>
       </div>
     
-      {/* <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid getRowId={(row) =>  row.s_id + row.s_name + row.makh + row.s_number + row.s_date}
-        className={styles.datagrid}
-        rows={tableData}
-        columns={columns.concat(actionColumn)}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 7,
-            },
-          },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box> */}
-    
     <TableContainer component={Paper} className={styles.table}>
       <Table sx={{ minWidth: 1200 }}  aria-label="a dense table">
         <TableHead>
           <TableRow>
-          <TableCell className={styles.tableCell+ " text-center"}>STT</TableCell>
-          <TableCell className={styles.tableCell+ " text-center"}>Mã Loại Dịch Vụ</TableCell>
-            <TableCell className={styles.tableCell+ " text-center"}>Tên Loại Dịch Vụ</TableCell>
-            <TableCell className={styles.tableCell+ " text-center"}>Giá</TableCell>
-            <TableCell className={styles.tableCell+ " text-center"}>Lựa Chọn</TableCell>
+          <TableCell className={styles.tableCell+ " text-center"}>STT</TableCell>{' '}
+          <TableCell className={styles.tableCell+ " text-center"}>Mã Loại Dịch Vụ</TableCell>{' '}
+            <TableCell className={styles.tableCell+ " text-center"}>Tên Loại Dịch Vụ</TableCell>{' '}
+            <TableCell className={styles.tableCell+ " text-center"}>Giá</TableCell>{' '}
+            <TableCell className={styles.tableCell+ " text-center"}>Lựa Chọn</TableCell>{' '}
           
             
           </TableRow>
@@ -135,18 +92,18 @@ const Service = () => {
               key={tableDataSVT.svt_id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell className={styles.tableCell+ " text-center"} >{index +1}</TableCell>
-              <TableCell className={styles.tableCell+ " text-center"}>{tableDataSVT.svt_id}</TableCell>
-              <TableCell className={styles.tableCell+ " text-center"}>{tableDataSVT.svt_name}</TableCell>
-              <TableCell className={styles.tableCell+ " text-center"}>{tableDataSVT.svt_price} VNĐ</TableCell>
+              <TableCell className={styles.tableCell+ " text-center"} >{index +1}</TableCell>{' '}
+              <TableCell className={styles.tableCell+ " text-center"}>{tableDataSVT.svt_id}</TableCell>{' '}
+              <TableCell className={styles.tableCell+ " text-center"}>{tableDataSVT.svt_name}</TableCell>{' '}
+              <TableCell className={styles.tableCell+ " text-center"}>{tableDataSVT.svt_price} VNĐ</TableCell>{' '}
 
               <TableCell className={styles.tableCell+ " text-center"}>     
               <div className={styles.cellAction}>
         <Link to={`/serviceType/adjustServiceType/${tableDataSVT.svt_id}`} style={{ textDecoration: "none" }}>
           <div className={styles.viewButton}>Edit</div>
-        </Link>
-      <Button className={styles.deleteButton}>Delete</Button>
-              </div></TableCell>
+        </Link>{' '}
+      <Button onClick={() => deleteSVT( tableDataSVT.svt_id )} className={styles.deleteButton}>Delete</Button>{' '}
+              </div></TableCell>{' '}
      
             </TableRow>
           ))}
@@ -215,7 +172,7 @@ const Service = () => {
         <Link to={`/service/view/${tableData.s_id}`} style={{ textDecoration: "none" }}>
           <div className={styles.viewButton}>View</div>
         </Link>
-      <Button className={styles.deleteButton}>Delete</Button>
+      <Button  className={styles.deleteButton}>Delete</Button>{" "}
               </div></TableCell>
      
             </TableRow>
