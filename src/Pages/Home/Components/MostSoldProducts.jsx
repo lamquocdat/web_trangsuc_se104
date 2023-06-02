a0282ebd293c9f2c2cacc1ef6d2275ca0ed3ec5e
anvuvip2002 <nguyenphuocanvu@gmail.com>
2023-06-02 23:47:20 +0700

    api trang home so san pham da ban

--- src/Pages/Home/Components/MostSoldProducts.jsx
+++ src/Pages/Home/Components/MostSoldProducts.jsx
@@ -12,20 +12,23 @@ function ProductHomepage({ products }) {
         <Row className="mx-4 mt-5">
           {/* visibleProducts = 12 */}
           {visibleProducts.map((product) => (
-            <Col key={product.id} sm={6} md={4} lg={3}>
+            <Col key={product.productid} sm={6} md={4} lg={3}>
               <Link to={`/productsdetail/${product._id}`}>
                 <Card style={{ background: "#f7f7f7", marginBottom: "30px" }}>
                   <Card.Body style={{ textAlign: "center" }}>
-                    <Card.Img src={product.productimage}></Card.Img>
+                    <Card.Img src={product.image}></Card.Img>
                     <Card.Title style={{ fontWeight: "200" }}>
                       {product.name}
                     </Card.Title>
                     <Card.Text style={{ color: "#e7b475", fontWeight: "500" }}>
-                      {product.price}
+                      {product.price} VNĐ
                     </Card.Text>
-                    <Card.Text style={{ textAlign: "center", fontSize: "15px" }}>
-                      {product.quantity_sold}
+                    <Card.Text style={{ color: "black", fontWeight: "300" }}>
+                      {product.quantity_sold} đã bán !
                     </Card.Text>
+                    {/* <Card.Text style={{ textAlign: "center", fontSize: "15px" }}>
+                      {product.sold}
+                    </Card.Text> */}
                   </Card.Body>
                 </Card>
               </Link>
