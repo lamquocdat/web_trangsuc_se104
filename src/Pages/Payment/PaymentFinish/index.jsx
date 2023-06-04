a71b06c09339530f567f7316ee79fd4b81fed512
gameitthoi <lamdat3302@gmail.com>
2023-06-04 16:48:40 +0700

    sua navigate cua paymentfinish

--- src/Pages/Payment/PaymentFinish/index.jsx
+++ src/Pages/Payment/PaymentFinish/index.jsx
@@ -16,7 +16,7 @@ function PaymentFinish () {
             <h1 className="text-success font-bold mb-3">Thông tin thanh toán của bạn đã được ghi nhận</h1>
             <p className="mb-3">Chúng tôi sẽ gửi xác nhận vào email cho bạn khi việc thanh toán được hoàn tất</p>
             <div className="my-3 py-3">
-                <Link to="#" className="text-primary">Nhấp vào đây để biết chi tiết đơn hàng</Link>
+                <Link to={`/orders/${localStorage.getItem('_id')}`} className="text-primary">Nhấp vào đây để biết chi tiết đơn hàng</Link>
             </div>
             <div className="my-3">
                 <Link to="/" className="border border-2 rounded px-3 py-3 mx-auto bg-primary text-white" style={{textDecoration: 'none'}}>Tiếp tục mua hàng</Link>
